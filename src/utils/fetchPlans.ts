8adc0cc505912adceeb1a161f355fce160eb30b2
Paul Chris Luke <paulchrisluke@gmail.com>
2025-12-17 14:39:19 +0800

    fix(plans): use unauthenticated client for public plans

--- src/utils/fetchPlans.ts
+++ src/utils/fetchPlans.ts
@@ -1,4 +1,4 @@
-import { apiClient } from '../lib/apiClient';
+import { publicApiClient } from '../lib/apiClient';
 
 export interface SubscriptionPlan {
   id: string;
@@ -28,7 +28,7 @@ export interface SubscriptionPlan {
 
 export const fetchPlans = async (): Promise<SubscriptionPlan[]> => {
   try {
-    const response = await apiClient.get('/api/subscriptions/plans');
+    const response = await publicApiClient.get('/api/subscriptions/plans');
     const plans = response.data?.plans || [];
     
     // Transform snake_case API response to camelCase TypeScript interface
