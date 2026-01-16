3fbb85d47cd807ab81d1657ec6778b7eb6e35841
Paul Chris Luke <paulchrisluke@gmail.com>
2026-01-16 20:02:57 +0800

    Add notifications e2e and update OneSignal worker

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,5 +1,1 @@
-try {
-  importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
-} catch (error) {
-  console.error('[OneSignal] Failed to load service worker SDK', error);
-}
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
